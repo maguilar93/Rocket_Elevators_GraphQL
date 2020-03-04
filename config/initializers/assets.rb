00bc267c2116a21b8cdcf163dddea4c9337c50ae
RdaBouazzaoui <reda.bouazaoui10@gmail.com>
2020-03-03 20:07:16 -0500

    fixTheError

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,9 +12,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-<<<<<<< HEAD
+
 Rails.application.config.assets.precompile += %w( admin.js admin.css )
-=======
+
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( admin.css )
 Rails.application.config.assets.precompile += %w( essentials.css )
@@ -23,4 +23,4 @@ Rails.application.config.assets.precompile += %w( header-1.css )
 Rails.application.config.assets.precompile += %w( blue.css )
 Rails.application.config.assets.precompile += %w( scripts.js )
 Rails.application.config.assets.precompile += %w( calculator.js )
->>>>>>> 990061974e3ee473db762a1037d04c037c653d06
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :pages
-<<<<<<< HEAD
+
 
   root 'pages#first'
-=======
+
   root 'pages#quoteform'
->>>>>>> 990061974e3ee473db762a1037d04c037c653d06
+
 
   get 'pages/first'
   get 'pages/second'
