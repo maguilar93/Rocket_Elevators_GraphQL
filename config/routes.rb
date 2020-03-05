a7e2392f274260e8166b7181d4e74cb01232b064
Kamiwackii <jackie.aki.lai@gmail.com>
2020-03-06 08:44:02 +0900

    removed useless pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
 
   resources :pages
   
-  root 'pages#first'
+  root 'pages#index'
 
-  get 'first' => 'pages#first' # index
-  get 'second' => 'pages#second' # corporate
-  get 'third' => 'pages#third' # residential
+  get 'index' => 'pages#index' # index
+  get 'corporate' => 'pages#corporate' # corporate
+  get 'residential' => 'pages#residential' # residential
   get 'quoteform' => 'pages#quoteform' # quote form
-
+  get 'login' => 'pages#login'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
