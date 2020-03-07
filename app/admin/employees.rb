6690a5216aba723243b35f74a3d14db2a1d2d466
Kamiwackii <jackie.aki.lai@gmail.com>
2020-03-07 12:28:27 +0900

    last commit v1

--- app/admin/employees.rb
+++ app/admin/employees.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Employee do
-  permit_params :email, :password, :password_confirmation
+  permit_params :first_name, :last_name, :email, :password, :password_confirmation
 
   index do
     selectable_column
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_07_020411) do
+ActiveRecord::Schema.define(version: 2020_03_07_012935) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -24,11 +24,6 @@ ActiveRecord::Schema.define(version: 2020_03_07_020411) do
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
-
-  create_table "customers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
