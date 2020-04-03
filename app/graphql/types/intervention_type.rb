4fd324331e24fdcc447ecd50e04f65b3334de369
kirilkk9 <kirilkleinerman@gmail.com>
2020-04-03 14:25:33 -0400

    graphql almost working, connected to PG

--- app/graphql/types/intervention_type.rb
+++ app/graphql/types/intervention_type.rb
@@ -1,4 +1,10 @@
 module Types
   class InterventionType < Types::BaseObject
+    field :id, ID, null: false
+    field :employeeId, Int, null: false, method: :employee_id
+    field :buildingId, Int, null: false, method: :building_id
+    field :building, BuildingType, null: false
+    field :dateStartIntervention, String, null: false, method: :date_start_intervention
+    field :dateEndIntervention, String, null: false, method: :date_end_intervention
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,29 +2,43 @@ module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
+    $conn = PG.connect(host: "codeboxx-postgresql.cq6zrczewpu2.us-east-1.rds.amazonaws.com", port: "5432", dbname:"mariaaguilar", user: "codeboxx", password: "Codeboxx1!")
+
+    field :intervention, InterventionType, null: false do
+      argument :id, ID, required: false
+    end
+    def intervention(id:)
+      res = $conn.exec("SELECT * FROM fact_intervention where employee_id = #{id}")
+      intervention = res[id.to_i]
+      building = Building.find(intervention["building_id"])
+      address = Address.find(building.address_id)
+      intervention[:building_address] = address
+      return intervention
+    end
+    
     #Building Query
     field :buildings, [Types::BuildingType], null: false
     def buildings
       Building.all
     end
-    field :building, Types::BuildingType, null: false do
-      argument :id, ID, required: true
-    end
-    def building(id:)
-      Building.find(id)
-    end
+    # field :building, Types::BuildingType, null: false do
+    #   argument :id, ID, required: true
+    # end
+    # def building(id:)
+    #   Building.find(id)
+    # end
 
     #Address Query
     field :addresses, [Types::AddressType], null: false
     def addresses
       Address.all
     end
-    field :address, Types::AddressType, null: false do
-      argument :id, ID, required: true
-    end
-    def address(id:)
-      Address.find(id)
-    end
+    # field :address, Types::AddressType, null: false do
+    #   argument :id, ID, required: true
+    # end
+    # def address(id:)
+    #   Address.find(id)
+    # end
 
     #Building Details Query
     field :buildingsdetails, [Types::BuildingdetailsType], null: false
@@ -38,7 +52,7 @@ module Types
       BuildingDetail.find(id)
     end
 
-    #Battery Query
+    # #Battery Query
     field :battery, [Types::BatteryType], null: false do
       argument :id, ID, required: true
     end
@@ -50,18 +64,18 @@ module Types
       Battery.all
     end
 
-    #Column Query
-    field :column , [Types::ColumnType] , null: false do
-      argument :id, ID, required: true
-    end
-    def column(id:)
-      Column.find(id)
-    end
-    end
-    field :columns, Types::ColumnType, null: false
-    def columns
-      Column.all
-    end
+    # #Column Query
+    # field :column, [Types::ColumnType], null: false do
+    #   argument :id, ID, required: true
+    # end
+    # def column(id:)
+    #   Column.find(id)
+    # end
+    # end
+    # field :columns, Types::ColumnType, null: false
+    # def columns
+    #   Column.all
+    # end
 
 
     def elevators
